147688ede19817271660098c201b9430498350d9
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-09-17 15:43:49 +0900

    バリデーション、アソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /[@]/ }
-  validates :password, presence: true, format: { with: /[a-z\d]{6, }/i }
+  validates :password, presence: true, length:{ minimum: 6 }
   validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :first_name,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :family_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
   validates :birth, presence: true
+
+  has_many  :items
 end
