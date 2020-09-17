1f3bd645ab039966c5abc6873b9f9f351f11e775
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-09-17 10:01:23 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /[@]/ }
+  validates :password, presence: true, format: { with: /[a-z\d]{6, }/i }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
+  validates :birth, presence: true
 end
