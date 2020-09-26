00a3d6a31a8d846ea1b73963912c9bdee1659194
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-09-26 22:33:57 +0900

    addressテーブルの修正

--- db/migrate/20200925094254_create_addresses.rb
+++ db/migrate/20200925094254_create_addresses.rb
@@ -3,10 +3,11 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
     create_table :addresses do |t|
 
       t.string :postal_code,    default: "",  null: false
-      t.integer :prefecture_id,                  null: false
+      t.integer :prefecture,                  null: false
       t.string :city,           default: ""
       t.string :house_number,   default: ""
       t.string :building_name,  default: ""
+      t.string :phone_number ,  default: ""
       t.references :order,                     null: false,  foreign_key: true
       t.timestamps
     end
