528606216232c8c8aeff609fdafb9ae0349d08f6
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-09-27 11:42:37 +0900

    indexビューの表示(仮)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,15 @@
 class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order = OrderAddress.new(order_params)
+  end
+
+  def create
+    binding.pry
+  end
+
+  private
+  def order_params
+    params.permit(:item_id, :postal_code, :prefecture, :city, :house_number, :builiding_name, :phone_number).merge(user_id: current_user.id)
+  end
 end
--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   
-  include ActiveModel::ActiveModel
+  include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number
 
   with_options presence: true do
