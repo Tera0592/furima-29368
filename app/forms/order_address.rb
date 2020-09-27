006101e8389a37d2cad06544fe0a44f66f38fb36
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-09-27 19:03:43 +0900

    バリデーションの修正(1)

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -7,7 +7,6 @@ class OrderAddress
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :house_number
-    validates :building_name
     validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "is invalid. Include hyphen(-)"}
   end
   validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
