101ea705929540a75d5500a75cdf3c13806b043b
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-09-28 16:48:59 +0900

    微修正とindexのsoldoutを表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,23 +10,24 @@ class OrdersController < ApplicationController
     if @order.valid?
       pay_item
       @order.save
-      return redirect_to root_path
+      redirect_to root_path
     else
       render 'index'
     end
   end
 
   private
+
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture, :city, :house_number, :builiding_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+
   def pay_item
-    Payjp.api_key = "sk_test_5dd3e963a14601d79dc3f2c4"
+    Payjp.api_key = 'sk_test_5dd3e963a14601d79dc3f2c4'
     Payjp::Charge.create(
-     amount: @item.price,
-     card: order_params[:token],
-     currency:'jpy'
-   )
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
-  
 end
