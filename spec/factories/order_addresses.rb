1d64c8c153a9f6adf84b4106c4ad907f821fae0f
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-09-28 17:27:43 +0900

    FactoryBotの記述(order)

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code { '123-4567' }
+    prefecture { Faker::Number.within(range: 1..47) }
+    city { '横浜市緑区' }
+    house_number { '青山1-1-1' }
+    phone_number { 09012345678 }
+    association :item
+    association :user
+  end
+end
