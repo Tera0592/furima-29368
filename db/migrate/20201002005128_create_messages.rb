f8de1a2804303f545f92b13d7319b1ae44ca8acf
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-10-02 10:06:19 +0900

    テーブル作成

--- db/migrate/20201002005128_create_messages.rb
+++ db/migrate/20201002005128_create_messages.rb
@@ -2,6 +2,9 @@ class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
 
+      t.integer :user_id,      null: false, foreign_key: true
+      t.integer :item_id,      null: false, foreign_key: true
+      t.text    :text,         null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_094254) do
+ActiveRecord::Schema.define(version: 2020_10_02_005128) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -56,6 +56,14 @@ ActiveRecord::Schema.define(version: 2020_09_25_094254) do
     t.integer "fee_id", null: false
     t.integer "place_id", null: false
     t.integer "day_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
+    t.text "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
