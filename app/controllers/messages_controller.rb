a1028b1f5df90144992ddbebb00cd4e5b6ecb337
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-10-13 09:16:06 +0900

    createアクション

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,7 @@ class MessagesController < ApplicationController
     @messages = Message.all
     @message = Message.new
   end
+  def create
+    @message = Message.new(text: params[:message][:text])
+  end
 end
